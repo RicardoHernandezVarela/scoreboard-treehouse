30eb578c8fe2be53f586a3d5ba558266b8f768d4
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-01-04 17:52:12 -0600

    Componentes Stats y Stopwatch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 /* Import components */
-//import Stats from './Stats';
-//import Stopwatch from './Stopwatch';
+import Stats from './Stats';
+import Stopwatch from './Stopwatch';
 
 const Header = () => {
     return (
       <header>
-        
+        <Stats />
         <h1>Scoreboard</h1>
-        
+        <Stopwatch />
       </header>
     );
   }
---
+++ src/components/Stats.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+/* Import context consumer */
+import { Consumer } from '../context/context';
+
+const Stats = () => {
+
+  return (
+    <Consumer>
+      { context => {
+
+        const totalPlayers = context.players.length;
+
+        /* Get total score from all players */
+        const totalPoints = context.players.reduce( (total, player) => {
+          return total + player.score;
+        }, 0);
+
+        return (
+          <table className="stats">
+            <tbody>
+              <tr>
+                <td>Players:</td>
+                <td>{ totalPlayers }</td>
+              </tr>
+              <tr>
+                <td>Total Points:</td>
+                <td>{ totalPoints }</td>
+              </tr>
+            </tbody>
+          </table>
+        );
+
+      }}
+    </Consumer>
+
+  );
+}
+
+export default Stats;
